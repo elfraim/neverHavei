fc4a0c8ff5a269e8052ae598299dcc8efa63d8b9
Elan Fraiman <elan@elfraiman.com>
2018-05-23 07:09:12 +0200

    Added questions

--- src/data/questions.js
+++ src/data/questions.js
@@ -4,9 +4,31 @@ export const Questions = [
     "set my or someone else’s hair on fire on purpose.",
     "had a bad fall because I was walking and texting.",
     "shot a gun",
-    "cheated on a test."
+    "cheated on a test.",
+    "broken something at a friend’s house and then not told them",
+    "shoplifted",
+    "broken a bone",
+    "been trapped in an elevator",
+    "been on TV or the radio",
+    "thrown up on a roller coaster",
+    "had a tree house",
+    "lied about a family member dying as an excuse to get out of doing something",
+    "been electrocuted",
+    "thought I was going to drown",
+    "driven drunk",
+    "Lied in this game",
+    "Stuck gum under a desk",
+
+
 ]
 
 export const TrapQuestions = [
-    'TRAP! Take a shot!', 'TRAP! Take a sip!', 'TRAP! Group dare!', 'TRAP! Group truth!', 'TRAP! Take 3 sips!'
+    'TRAP! Take a shot!',
+    'TRAP! Group dare!',
+    'TRAP! Group truth!',
+    'TRAP! Would you rather Win $50,000 or let your best friend win $500,000?',
+    'TRAP! Would you rather know how you will die or when you will die?',
+    'TRAP! Would you rather speak all languages or be able to speak to all animals?',
+    
+
 ]
